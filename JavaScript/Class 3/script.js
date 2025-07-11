7ef1e6c23ff31b30bf8d29f7e96c1abc49720573
Manas Ranjan Bera <manasbera484@gmail.com>
2025-07-11 18:31:30 +0530

    Array inbuilt methods

---
+++ JavaScript/Class 3/script.js
@@ -0,0 +1,39 @@
+//! Array inbuilt methods
+let arr = [100, 20, 40, true, false, undefined, "Hello", null]
+//? push()
+// arr.push(1, 10)
+// console.log(arr);
+
+//? pop()
+// arr.pop()
+// console.log(arr);
+
+//? unshift()
+// arr.unshift(false, "Hi", "Good Evening")
+// console.log(arr);
+
+//? shift()
+// arr.shift()
+// console.log(arr);
+
+//? slice()
+// let subArr = arr.slice(2, 6)
+// console.log(subArr);
+
+//? reverse()
+// arr.reverse()
+// console.log(arr);
+
+//? splice()
+// arr.splice(1, 2) // delete
+// console.log(arr);
+// arr.splice(1, 0, 10, 20, 30) //add
+// console.log(arr);
+// arr.splice(2, 1, true, undefined) // replace
+// console.log(arr);
+
+//? toString()
+// console.log(arr.toString());
+
+//? includes()
+console.log(arr.includes(100));
